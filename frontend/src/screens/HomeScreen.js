360fefda174e25f132581db56b0e0d03a9b7a835
Ekmel Kalayci <ekmkal@gmail.com>
2021-07-29 20:44:05 +0200

    alphabetic,length,suffix filtering features added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,6 +7,7 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Meta from '../components/Meta';
 import { listMyWords, updateWords } from '../actions/wordActions';
+import { WORD_UPDATE_RESET } from '../constants/wordConstants';
 
 const HomeScreen = ({ history }) => {
   const dispatch = useDispatch();
@@ -15,42 +16,129 @@ const HomeScreen = ({ history }) => {
   const [myNewWords, setMyNewWords] = useState([]);
   const [text, setText] = useState('');
   const [wordsNotInStore, setWordsNotInStore] = useState([]);
+  const [knownWordCount, setKnownWordCount] = useState(0);
+  const [noText, setNoText] = useState(false);
 
   const wordListMy = useSelector((state) => state.wordListMy);
   const { loading, error, myWords } = wordListMy;
+
+  const wordUpdate = useSelector((state) => state.wordUpdate);
+  const { success } = wordUpdate;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
   useEffect(() => {
-    if (!myWords) {
+    if (!myWords || success) {
+      dispatch({ type: WORD_UPDATE_RESET });
+
       dispatch(listMyWords());
     } else {
       history.push('/');
       setMyWordsInState(myWords);
     }
-  }, [dispatch, history, myWords, userInfo]);
+  }, [dispatch, history, myWords, userInfo, success]);
+
+  const suffixes = [
+    's',
+    'es',
+    'ing',
+    'd',
+    'ed',
+    'ly',
+    'er',
+    'r',
+    'st',
+    'tion',
+    'ion',
+    'y',
+    'en',
+    'ment',
+    'able',
+    'ity',
+    'lity',
+    'al',
+    'n',
+    'less',
+    'ness',
+    'est',
+    'ful',
+    'fully',
+  ];
 
   const extractText = (e) => {
     e.preventDefault();
-    const extractedWordsArr = [];
-    // eslint-disable-next-line array-callback-return
-    text.split(' ').map((word) => {
-      if (!myWordsInState.includes(word)) {
-        console.log(word);
-        extractedWordsArr.push(word);
+    if (text.split(' ').length === 1 && text.split(' ')[0] === '') {
+      setNoText(true);
+      setWordsNotInStore([]);
+    } else {
+      setNoText(false);
+      setWordsNotInStore([]);
+      setKnownWordCount(0);
+      const extractedWordsArr = [];
+      let count = 0;
+      const textWordsArr = text
+        .split(' ')
+        .filter((n) => n !== '' && !/[^a-zA-Z]/.test(n) && n.length > 2);
+      for (let suffix of suffixes) {
+        // eslint-disable-next-line array-callback-return
+        // eslint-disable-next-line no-loop-func
+        textWordsArr.map((m) => {
+          if (textWordsArr.includes(m) && textWordsArr.includes(m + suffix)) {
+            const index = textWordsArr.indexOf(m + suffix);
+            if (index !== -1) {
+              count += 1;
+              textWordsArr.splice(index, 1);
+            }
+          }
+        });
+        for (let word of myWordsInState) {
+          // eslint-disable-next-line array-callback-return
+          // eslint-disable-next-line no-loop-func
+          textWordsArr.map((k) => {
+            if (k === word + suffix) {
+              const index = textWordsArr.indexOf(k);
+              if (index !== -1) {
+                count += 1;
+                textWordsArr.splice(index, 1);
+              }
+            }
+          });
+        }
       }
-    });
-    setWordsNotInStore(extractedWordsArr);
+      console.log(textWordsArr);
+      // eslint-disable-next-line array-callback-return
+      textWordsArr.map((word) => {
+        if (!myWordsInState.includes(word) && !extractedWordsArr.includes(word)) {
+          extractedWordsArr.push(word);
+        } else {
+          count += 1;
+        }
+      });
+      setWordsNotInStore(extractedWordsArr);
+      setKnownWordCount(count);
+    }
   };
 
   const addNewWord = (n) => {
     setMyNewWords([...myNewWords, n]);
   };
 
+  const removeNewWord = (n) => {
+    const updatedNewWordsArr = [...myNewWords];
+    console.log(updatedNewWordsArr);
+    const index = updatedNewWordsArr.indexOf(n);
+    console.log(index);
+    if (index !== -1) {
+      updatedNewWordsArr.splice(index, 1);
+      setMyNewWords(updatedNewWordsArr);
+    }
+  };
+
   const updateHandler = () => {
     if (window.confirm('Are you sure to add these words to your store?')) {
       dispatch(updateWords([...myWords, ...myNewWords]));
+      setMyNewWords([]);
     }
   };
 
@@ -74,6 +162,7 @@ const HomeScreen = ({ history }) => {
         <Col sm="6">
           <Form onSubmit={extractText}>
             <Form.Label>Input the text you want to extract</Form.Label>
+            {noText && <Message variant="warning">Please input some text..</Message>}
             <Form.Group className="mb-3" controlId="inputText">
               <Form.Control
                 type="text"
@@ -88,31 +177,44 @@ const HomeScreen = ({ history }) => {
             </Button>
           </Form>
         </Col>
-        <Col sm="6">
-          <Table striped bordered hover responsive className="table-sm">
-            <thead>
-              <tr>
-                <th>The Words Not In Your Store</th>
-                <th>ADD TO MY STORE</th>
-              </tr>
-            </thead>
-            <tbody>
-              {wordsNotInStore.map((word, index) => (
-                <tr key={index}>
-                  <td>{word}</td>
-                  <td>
-                    <Button variant="danger" className="btn-sm" onClick={() => addNewWord(word)}>
-                      <i className="fas fa-plus"></i>
-                    </Button>
-                  </td>
+        {wordsNotInStore.length !== 0 && (
+          <Col sm="6">
+            <h3>Known Words Count: {knownWordCount}</h3>
+            <Table striped bordered hover responsive className="table-sm">
+              <thead>
+                <tr>
+                  <th>The Words Not In Your Store: {wordsNotInStore.length}</th>
+                  <th>ADD TO MY STORE</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-          <Button type="submit" variant="warning" onClick={updateHandler}>
-            Update My Words
-          </Button>
-        </Col>
+              </thead>
+              <tbody>
+                {wordsNotInStore.map((word, index) => (
+                  <tr key={index}>
+                    <td>{word}</td>
+                    <td>
+                      {!myNewWords.includes(word) ? (
+                        <Button variant="info" className="btn-sm" onClick={() => addNewWord(word)}>
+                          <i className="fas fa-plus"></i>
+                        </Button>
+                      ) : (
+                        <Button
+                          variant="danger"
+                          className="btn-sm"
+                          onClick={() => removeNewWord(word)}
+                        >
+                          <i className="fas fa-trash"></i>
+                        </Button>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            <Button type="submit" variant="warning" onClick={() => updateHandler}>
+              Update My Words
+            </Button>
+          </Col>
+        )}
       </Row>
     </>
   );
